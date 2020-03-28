74fd13fd4f73da3ad4b866e6893b19adfb2d4508
VishnuprakashSelvarajan <svishnuprakash@gmail.com>
2020-03-28 02:02:00 -0700

    Given two lists of closed intervals, each list of intervals is pairwise disjoint and in sorted order.

---
+++ Interval_list_intersections.py
@@ -0,0 +1,48 @@
+"""
+Given two lists of closed intervals, each list of intervals is pairwise disjoint and in sorted order.
+
+Return the intersection of these two interval lists.
+
+(Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.  The intersection of two closed intervals is a set of real numbers that is either empty, or can be represented as a closed interval.  For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
+
+
+
+Example 1:
+
+
+
+Input: A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
+Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+Reminder: The inputs and the desired output are lists of Interval objects, and not arrays or lists.
+
+
+Note:
+
+0 <= A.length < 1000
+0 <= B.length < 1000
+0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9
+"""
+
+def intervalIntersection(A,B):
+    output = []
+
+    a_index, b_index = 0, 0
+    while b_index < len(B) and a_index < len(A):
+        if A[a_index][1] < B[b_index][0]:
+            a_index += 1
+        elif A[a_index][0] > B[b_index][1]:
+            b_index += 1
+        else:
+            a = max(A[a_index][0], B[b_index][0])  # start
+            if A[a_index][1] <= B[b_index][1]:
+                b = A[a_index][1]  # end
+                a_index += 1
+            else:
+                b = B[b_index][1]  # end
+                b_index += 1
+            output.append([a, b])
+
+    return output
+
+
+print(intervalIntersection([[0,2],[5,10],[13,23],[24,25]], [[1,5],[8,12],[15,24],[25,26]]))

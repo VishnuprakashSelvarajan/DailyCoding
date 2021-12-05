ac02d0b2098c0da2cafd29a7802ed4f630e8f8a7
VishnuprakashSelvarajan <svishnuprakash@gmail.com>
2021-12-05 10:29:11 -0800

    New Palindrome Methods

--- Palindrome.py
+++ Palindrome.py
@@ -1,22 +1,39 @@
 def IsPalindrome(text):
 
-    def IsEqual(sub_text):
-        if sub_text[0] == sub_text[-1]:
-            return 1
-        return -1
+    if len(text) == 2:
+        return [True if text[0] == text[1] else False]
+    if len(text) < 2:
+        return True
 
-    start = 0
-    end = len(text)-1
+
+
     text = list(text)
     while len(text) > 1:
-        if IsEqual(text):
-            text.pop(start)
-            text.pop(end)
-            start +=1
-            end -=1
+        if text[0] == text[-1]:
+            text.pop(0)
+            text.pop(-1)
+            IsPalindrome(text)
         else:
             return False
 
+
     return True
 
-print(IsPalindrome('aba'))
+print(IsPalindrome('aaaaaabb'))
+
+def powerset(arr):
+
+    def all_subset(arr, subset):
+        while len(arr) > 0:
+            subset.append(arr[0:len(arr)])
+            arr.pop(-1)
+            powerset(arr)
+        return subset
+
+    subsets = []
+    for i in range(0,len(arr)):
+        subsets.append(all_subset(arr[i:len(arr)], []))
+
+    return subsets
+print(powerset([1,2,3,4,5,6,7,8,9]))
+
